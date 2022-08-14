d526b0cad55b61a8d77c887f97aa3e3dd1470b5b
Abhishek Bhatt <abhi.bhatt100@gmail.com>
2022-08-14 17:08:55 -0400

    Update OauthTest.java

--- src/test/OauthTest.java
+++ src/test/OauthTest.java
@@ -7,7 +7,7 @@ import static io.restassured.RestAssured.given;
 public class OauthTest {
 
     public static void main(String[] args) throws InterruptedException {
-        
+
 
         String url="https://oauth.pstmn.io/v1/callback?state=verify&code=4/0AdQt8qjRxJKy3AhCHG4gb0cOjbv42RCxLxLIBjdqImpNWsgxf_" +
                 "MVZZyW03X6u70tHwYnZw&scope=https://www.googleapis.com/auth/gmail.modify";
@@ -19,7 +19,7 @@ public class OauthTest {
         String accessTokenResponse = given().urlEncodingEnabled(false).queryParam("code",code)
           .queryParam("code","")
                .queryParam("client_id","532618568160-t8043mcfebav92pd25sus1g6ifof25em.apps.googleusercontent.com")
-               .queryParam("client_secret","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")
+               .queryParam("client_secret","XXXXXXXXXXXXXXXXXXXXXXXXX") // have put as such for security reasons
                .queryParam("redirect_uri","https://oauth.pstmn.io/v1/callback")
                .queryParam("grant_type","authorization_code")
                .when().log().all().post("https://accounts.google.com/o/oauth2/token").asString();
