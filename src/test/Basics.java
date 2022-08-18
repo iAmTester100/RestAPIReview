1d9fc328f3d1d8e141a3bde54603e93f2338b104
Abhishek Bhatt <abhi.bhatt100@gmail.com>
2022-08-17 20:38:35 -0400

    Serialization/Deserialization for nested JSON

--- src/test/Basics.java
+++ src/test/Basics.java
@@ -3,8 +3,11 @@ package test;
 import files.ReusableMethods;
 import files.payload;
 import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
 import io.restassured.path.json.JsonPath;
 import org.testng.Assert;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -13,13 +16,13 @@ public class Basics {
 
     public static void main(String[] args) {
 
-        RestAssured.baseURI="https://reqres.in/";
+        RestAssured.baseURI = "https://reqres.in/";
 
-     String realName = "Abhishek";
+        String realName = "Abhishek";
 
-      String response = given().log().all().header("Connection","keep-alive").header("Content-Type","application/json")
+        String response = given().log().all().header("Connection", "keep-alive").header("Content-Type", "application/json")
                 .body(payload.addUser()).when().post("api/users")
-                .then().assertThat().statusCode(201).body("name",equalTo("Abhishek")).extract().response().asString();
+                .then().assertThat().statusCode(201).body("name", equalTo("Abhishek")).extract().response().asString();
 
         System.out.println(response);
 
@@ -27,18 +30,32 @@ public class Basics {
         String responseName = js.getString("name");
         System.out.println(response);
 
-        Assert.assertEquals(realName,responseName);
+        Assert.assertEquals(realName, responseName);
 
 
-        String getName = given().log().all().header("Connection","keep-alive").header("Content-Type","application/json")
-                .when().get("api/user/2")
-                .then().log().all().assertThat().statusCode(200).extract().response().asString();
+        GetUsers getPageList = given().log().all().header("Connection", "keep-alive").header("Content-Type", "application/json").expect().defaultParser(Parser.JSON)
+                .when().get("api/unknown").as(GetUsers.class);
 
 
+        System.out.println(getPageList.getPage());
+        System.out.println(getPageList.getPer_page());
+        System.out.println(getPageList.getData().get(0).getColor());
+        System.out.println(getPageList.getData().get(1).getId());
+        System.out.println(getPageList.getSupport().getText());
+        System.out.println(getPageList.getSupport().getUrl());
 
-     String updatedResponse=   given().log().all().header("Connection","keep-alive").header("Content-Type","application/json")
+        List<GetData> data = getPageList.getData();
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i).getName().equalsIgnoreCase("fuchsia rose")) {
+                System.out.println(data.get(i).getYear());
+            }
+
+        }
+
+
+        String updatedResponse = given().log().all().header("Connection", "keep-alive").header("Content-Type", "application/json")
                 .body(payload.updateUser()).when().put("api/users/2")
-                .then().log().all().assertThat().statusCode(200).body("name",equalTo("Ajay")).extract().response().asString();
+                .then().log().all().assertThat().statusCode(200).body("name", equalTo("Ajay")).extract().response().asString();
 
         System.out.println(updatedResponse);
 
@@ -47,7 +64,7 @@ public class Basics {
         System.out.println(newName);
 
 
-        given().log().all().header("Connection","keep-alive").header("Content-Type","application/json")
+        given().log().all().header("Connection", "keep-alive").header("Content-Type", "application/json")
                 .body(payload.deleteUser()).when().delete("api/users/2")
                 .then().log().all().assertThat().statusCode(204);
 
